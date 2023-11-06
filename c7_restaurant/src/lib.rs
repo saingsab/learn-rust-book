ab9cbe71b0a05b89206333c72c60c80d5b9c40af
Ayoung <saing.sab@gmail.com>
2023-11-06 23:07:05 +0700

    use scope

--- c7_restaurant/src/lib.rs
+++ c7_restaurant/src/lib.rs
@@ -1,6 +1,6 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
         fn seat_at_table() {}
     }
     mod serving {
@@ -8,6 +8,64 @@ mod front_of_house {
         fn serve_order() {}
         fn take_payment() {}
     }
+}
+
+fn deliver_order() {}
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast { 
+                toast: String::from(toast), 
+                seasonal_fruit: String::from("peaches") 
+            }
+        }
+    }
+
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order(); //This is like starting a filesystem path with the .. syntax.
+    }
+
+    fn cook_order() {}
+}
+
+// if we make an enum public, all of its variants are then public. We only need the pub before the enum keyword,
+mod another_back_of_house {
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+}
+
+// Bringing Paths into Scope with the use Keyword
+use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    // // Absolute path
+    // crate::front_of_house::hosting::add_to_waitlist();
+    hosting::add_to_waitlist(); //after declear use crate
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheet");
+    println!("I'd like {} toast please!", meal.toast);
+
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    // accessing pub enum
+    let order1 = another_back_of_house::Appetizer::Salad;
 }
 
 // crate
