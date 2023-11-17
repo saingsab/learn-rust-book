d1b9007c1a742f1c8ef2460c144b6dccc9c5ce22
Ayoung <saing.sab@gmail.com>
2023-11-17 11:56:18 +0700

    complete match

--- c18_matching/src/main.rs
+++ c18_matching/src/main.rs
@@ -1,10 +1,137 @@
+
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+// Destructuring Enums
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
 fn main() {
+    // Matching Literals
+    let x = 1;
+
+    match x {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+    // Matching Named Variables
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched, y = {y}"),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {y}", x);
+
+    // Multiple Patterns
+    let x = 1;
+    
+    match x {
+        1 | 2 => println!("one or two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+
+    // Matching Ranges of Values with ..=
+    let x = 5;
+    match x {
+        1..=5 => println!("one through five"),
+        _ => println!("something else"),
+    }
+
+    let x = 'c';
+
+    match x {
+        'a'..='j' => println!("early ASCII letter"),
+        'k'..='z' => println!("late ASCII letter"),
+        _ => println!("something else"),
+    }
+
+    // Destructuring to Break Apart Values
+    // Destructuring Structs
+    let p = Point {x: 0, y: 7};
+
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {x}"),
+        Point { x: 0, y} => println!("On the y axis at {y}"),
+        Point {x, y} => {
+            println!("On neither axis: ({x}, {y})");
+        }
+    }
+
+    // Destructuring Enums
+    let msg = Message::ChangeColor(0, 160, 255);
+
+    match msg {
+        Message::Quit => {
+            println!("The Quit variant has no data to destructure.");
+        }
+        Message::Move { x, y } => {
+            println!("Move in the x direction {x} and in the y direction {y}");
+        }
+        Message::Write(text) => {
+            println!("Text message: {text}"); 
+        }
+        Message::ChangeColor(r, g, b) => {
+            println!("Change the color to red {r}, green {g}, and blue {b}",)  
+        }
+    }
+
+    // Ignoring Parts of a Value with a Nested _
+    let mut setting_value = Some(5);
+    let new_setting_value = Some(10);
+
+    match (setting_value, new_setting_value) {
+        (Some(_), Some(_)) => {
+            println!("Can't overwrite an existing customized value");
+        }
+        _ => {
+            setting_value = new_setting_value;
+        }
+    }
+
+    println!("setting is {:?}", setting_value);
+
+    // Extra Conditionals with Match Guards
+    let num = Some(4);
+
+    match num {
+        Some(x) if x % 2 == 0 => println!("The number {} is even", x),
+        Some(x) => println!("The number {} is odd", x),
+        _ => (),
+    }
+
+    let x = Some(50);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(n) if n == y => println!("Matched, n = {n}"),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {y}", x);
+
+    // @ Bindings
+    
 //    match Arms
 // a value to match on, and one or more match arms that consist of a pattern and an expression to run if the value matches that arm’s pattern,
     //match x {
     //     None => None,
     //     Some(i) => Some(i + 1),
     // }
+
+
 
     // Conditional if let Expressions
     let favorite_color: Option<&str> = None;
